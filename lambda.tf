49e20e83101f78fa8095a00b16b3d237f2659eec
carlos  gonzalez <carlos@Mini-de-carlos.bbrouter>
2026-03-21 16:19:01 -0500

    Refactor: Move IAM resources from lambda.tf to iam.tf

--- lambda.tf
+++ lambda.tf
@@ -1,26 +1,3 @@
-# IAM Role for Lambda
-resource "aws_iam_role" "lambda_exec" {
-  name = "register-product-lambda-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Sid    = ""
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
-}
-
-# Basic execution policy for Lambda (logging to CloudWatch)
-resource "aws_iam_role_policy_attachment" "lambda_logs" {
-  role       = aws_iam_role.lambda_exec.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
 # Zip a placeholder bootstrap code
 data "archive_file" "lambda_zip" {
   type        = "zip"
