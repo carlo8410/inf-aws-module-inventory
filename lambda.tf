707e6587a6b007c2df5fcd6e9123b21f4f0f521b
carlos  gonzalez <carlos@Mini-de-carlos.bbrouter>
2026-03-21 16:11:05 -0500

    Create register-product Lambda function and IAM role

---
+++ lambda.tf
@@ -0,0 +1,47 @@
+# IAM Role for Lambda
+resource "aws_iam_role" "lambda_exec" {
+  name = "register-product-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+# Basic execution policy for Lambda (logging to CloudWatch)
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Zip the source code
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/src/index.js"
+  output_path = "${path.module}/src/index.zip"
+}
+
+# Lambda Function
+resource "aws_lambda_function" "register_product" {
+  filename      = data.archive_file.lambda_zip.output_path
+  function_name = "register-product"
+  role          = aws_iam_role.lambda_exec.arn
+  handler       = "index.handler"
+
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  runtime = "nodejs18.x"
+
+  environment {
+    variables = {
+      ENV = "dev"
+    }
+  }
+}
