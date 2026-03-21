3f4bdb31c17a27264d89faed7abdb4807adac2fc
carlos  gonzalez <carlos@Mini-de-carlos.bbrouter>
2026-03-21 17:09:38 -0500

    Implement Lambda aliases: latest () and v1 (versioned)

--- lambda.tf
+++ lambda.tf
@@ -6,9 +6,28 @@ resource "aws_lambda_function" "register_product" {
   handler          = "index.handler"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   runtime          = "nodejs24.x"
+  publish          = true
+
   environment {
     variables = {
       ENV = "dev"
     }
   }
 }
+
+# Alias pointing to the latest code ($LATEST)
+resource "aws_lambda_alias" "latest" {
+  name             = "latest"
+  description      = "Alias pointing to the latest code ($LATEST)"
+  function_name    = aws_lambda_function.register_product.function_name
+  function_version = "$LATEST"
+}
+
+# Alias pointing to a specific version (V1)
+resource "aws_lambda_alias" "v1" {
+  name             = "v1"
+  description      = "Alias for stable version 1"
+  function_name    = aws_lambda_function.register_product.function_name
+  function_version = aws_lambda_function.register_product.version
+}
+
