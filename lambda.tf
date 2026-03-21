7039b03121c6d2bfb6330e1fcc3b69c86400ff02
carlos  gonzalez <carlos@Mini-de-carlos.bbrouter>
2026-03-21 16:16:45 -0500

    Implement Lambda Bootstrap pattern and remove source code from infra repo

--- lambda.tf
+++ lambda.tf
@@ -21,11 +21,15 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-# Zip the source code
+# Zip a placeholder bootstrap code
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_file = "${path.module}/src/index.js"
-  output_path = "${path.module}/src/index.zip"
+  output_path = "${path.module}/lambda_function_payload.zip"
+
+  source {
+    content  = "exports.handler = async (event) => { console.warn('BOOTSTRAP: Please deploy real code'); return { statusCode: 200, body: JSON.stringify('Bootstrap response') }; };"
+    filename = "index.js"
+  }
 }
 
 # Lambda Function
