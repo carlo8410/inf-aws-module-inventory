bd5a598ad27ea03759720d392a6e306c3c08d2c4
carlos  gonzalez <carlos@Mini-de-carlos.bbrouter>
2026-03-21 12:16:46 -0500

    Delete s3.tf and manage state bucket manually

--- s3.tf
+++
@@ -1,3 +0,0 @@
-# Este archivo está vacío temporalmente o puedes añadir otros recursos.
-# El bucket 'inf-aws-module-inventory-state' se gestiona manualmente 
-# para evitar conflictos con el backend de Terraform.
