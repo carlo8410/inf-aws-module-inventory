e7faf78f619d709b6aa3938ad3bb110476ccc42e
carlos  gonzalez <carlos@Mini-de-carlos.bbrouter>
2026-03-21 17:03:02 -0500

    Fix: Remove deployment-related resources from infra repo to allow external management

--- api-gw.tf
+++ api-gw.tf
@@ -5,18 +5,6 @@ resource "aws_api_gateway_rest_api" "mc_inventory_api" {
     types = ["REGIONAL"]
   }
 }
-resource "aws_api_gateway_deployment" "mc_inventory_api_deployment" {
-  rest_api_id = aws_api_gateway_rest_api.mc_inventory_api.id
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-resource "aws_api_gateway_stage" "mc_inventory_api_stage_dev" {
-  deployment_id = aws_api_gateway_deployment.mc_inventory_api_deployment.id
-  rest_api_id   = aws_api_gateway_rest_api.mc_inventory_api.id
-  stage_name    = "dev"
-}
-
 # Outputs for the Application Repository
 output "api_gateway_id" {
   description = "The ID of the REST API Gateway to be used in the Lambda repo"
@@ -26,10 +14,5 @@ output "api_gateway_id" {
 output "api_gateway_execution_arn" {
   description = "The execution ARN of the REST API Gateway"
   value       = aws_api_gateway_rest_api.mc_inventory_api.execution_arn
-}
-
-output "api_url" {
-  description = "The base URL of the REST API Gateway"
-  value       = aws_api_gateway_stage.mc_inventory_api_stage_dev.invoke_url
 }
 
